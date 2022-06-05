1e9cd9d9f29f09d340d65ebc7b9129aa7e369cf7
Jakub <drwal.j04@htlwienwest.at>
2022-06-05 17:14:16 +0200

    Projektdateien hinzufügen.

---
+++ Drwal_SEW_Projekt_EF/Controllers/VoDController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Drwal_SEW_Projekt_EF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VoDController : ControllerBase
+    {
+        // GET: api/<VoDController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<VoDController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<VoDController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<VoDController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<VoDController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Vod_Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Vod_Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Uri uri = new Uri("https://localhost:7256/api/restaurant/%22);
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = uri;
+
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+        }
+    }
+}
