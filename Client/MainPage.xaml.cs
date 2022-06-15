ec3c8ee865463bc9f2367962c742cc995813c15a
Jakub <drwal.j04@htlwienwest.at>
2022-06-15 22:20:48 +0200

    Movies Suchen mit genres funktioniert jetzt

--- Client/MainPage.xaml.cs
+++ Client/MainPage.xaml.cs
@@ -75,7 +75,7 @@ namespace WPF_Client
             {
                 lb_Movies.Items.Clear();
 
-                if (tb_SearchMovie.Text != "Search...")
+                if (tb_SearchMovie.Text != "Search..." && cb_Genre.Text != "")
                 {
                     foreach (Movie m in await RestHelper.GetMoviesWithGenreAndTitleAsync(tb_SearchMovie.Text.ToString(), cb_Genre.Text))
                     {
@@ -83,8 +83,24 @@ namespace WPF_Client
                     }
 
                 }
+                else if (tb_SearchMovie.Text == "Search..." && cb_Genre.Text == "All")
+                {
+                    foreach (Movie m in await RestHelper.GetAllMoviesAsync())
+                    {
+                        lb_Movies.Items.Add(m.ToString());
+                    }
 
-                else if (tb_SearchMovie.Text == "Search..." || tb_SearchMovie.Text == "")
+                }
+                else if (tb_SearchMovie.Text == "Search..." && cb_Genre.Text != "All")
+                {
+                    foreach (Movie m in await RestHelper.GetMoviesWithGenreAndTitleAsync("", cb_Genre.Text))
+                    {
+                        lb_Movies.Items.Add(m.ToString());
+                    }
+
+                }
+
+                else if (tb_SearchMovie.Text == "" && cb_Genre.Text != "")
                 {
                     foreach (Movie m in await RestHelper.GetMoviesWithGenreAndTitleAsync(title: "", genre: cb_Genre.Text))
                     {
@@ -109,22 +125,41 @@ namespace WPF_Client
 
         private async void GenreBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+
             try
             {
+                var meow = (GenreEnum)cb_Genre.SelectedItem;
+                string text = meow.ToString();
                 lb_Movies.Items.Clear();
 
-                if (tb_SearchMovie.Text != "Search...")
+                if (tb_SearchMovie.Text != "Search..." && text != "")
                 {
-                    foreach (Movie m in await RestHelper.GetMoviesWithGenreAndTitleAsync(tb_SearchMovie.Text.ToString(), cb_Genre.Text))
+                    foreach (Movie m in await RestHelper.GetMoviesWithGenreAndTitleAsync(tb_SearchMovie.Text.ToString(), text))
+                    {
+                        lb_Movies.Items.Add(m.ToString());
+                    }
+
+                }
+                else if (tb_SearchMovie.Text == "Search..." && text == "All")
+                {
+                    foreach (Movie m in await RestHelper.GetAllMoviesAsync())
+                    {
+                        lb_Movies.Items.Add(m.ToString());
+                    }
+
+                }
+                else if (tb_SearchMovie.Text == "Search..." && text != "All")
+                {
+                    foreach (Movie m in await RestHelper.GetMoviesWithGenreAndTitleAsync("", text))
                     {
                         lb_Movies.Items.Add(m.ToString());
                     }
 
                 }
 
-                else if (tb_SearchMovie.Text == "Search..." || tb_SearchMovie.Text == "")
+                else if (tb_SearchMovie.Text == "" && text != "")
                 {
-                    foreach (Movie m in await RestHelper.GetMoviesWithGenreAndTitleAsync(title:"",genre: cb_Genre.Text))
+                    foreach (Movie m in await RestHelper.GetMoviesWithGenreAndTitleAsync("", text))
                     {
                         lb_Movies.Items.Add(m.ToString());
                     }
